b4c167b732dd6ae57046fa99d696f0eac43f3981
Camilo Andres Ferrer Bustos <caferrerb@gmail.com>
2021-04-26 20:24:23 -0500

    fix app

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
-// server.js
+//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
-  // For all GET requests, send back index.html
-// so that PathLocationStrategy can be used
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-  });
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+});
 
-
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
