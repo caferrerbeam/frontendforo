7b6eaefcdb7fa8bb9ae42a60efa449dd3451eb49
Camilo Andres Ferrer Bustos <caferrerb@gmail.com>
2021-04-26 20:15:21 -0500

    fix app

--- server.js
+++ server.js
@@ -4,25 +4,6 @@ const path = require('path');
 
 const app = express();
 
-// If an incoming request uses
-// a protocol other than HTTPS,
-// redirect that request to the
-// same url but with HTTPS
-const forceSSL = function() {
-    return function (req, res, next) {
-      if (req.headers['x-forwarded-proto'] !== 'https') {
-        return res.redirect(
-         ['https://', req.get('Host'), req.url].join('')
-        );
-      }
-      next();
-    }
-  }
-  // Instruct the app
-  // to use the forceSSL
-  // middleware
-  app.use(forceSSL());
-
 
   // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
